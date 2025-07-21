bf4e01bfaeec8bfe5047da5488530bb26a84021d
paurasm22 <150505687+paurasm22@users.noreply.github.com>
2025-07-21 20:09:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,9 @@ def predict():
 def home():
     return "Flask API is running!"
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
